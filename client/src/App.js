226da12e27ad5a0846d7209d6613f3d1f5ee96ed
nthoang74 <nthoang7497@gmail.com>
2022-06-29 12:05:30 +0700

    init project

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react"
+import Navbar from "./components/Navbar/Navbar"
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
+import Home from "./components/Home/Home"
+import Auth from "./components/Auth/Auth"
+import Footer from "./components/Footer/Footer"
+import QuizCreator from "./components/QuizCreator/QuizCreator"
+import Quizzes from "./components/Quizzes/Quizzes"
+import MyQuizzes from "./components/MyQuizzes/MyQuizzes"
+import QuizDetails from "./components/QuizDetails/QuizDetails"
+import HostScreen from "./components/Game/HostScreen/HostScreen"
+import PlayerScreen from "./components/Game/PlayerScreen/PlayerScreen"
+import JoinGame from "./components/Game/JoinGame/JoinGame"
+import { io } from "socket.io-client"
+import { useDispatch } from "react-redux"
+import { createSocket } from "./actions/socket"
+
+function App() {
+  const user = JSON.parse(localStorage.getItem("profile"))
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const socket = io("http://localhost:3001")
+    dispatch(createSocket(socket))
+
+    return () => socket.disconnect()
+  }, [dispatch])
+
+  return (
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={() => (user === null ? <Auth /> : <Redirect to="/" />)} />
+          <Route path="/quizes" exact component={Quizes} />
+          <Route path="/quizes/search" exact component={Quizes} />
+          <Route path="/quizes/:id" exact component={QuizDetails} />
+          <Route path="/myquizes/:id" exact component={QuizCreator} />
+          <Route path="/games/joingame" exact component={JoinGame} />
+          <Route path="/games/host/:id" exact component={HostScreen} />
+          <Route path="/games/player/:id" exact component={PlayerScreen} />
+          <Route path="/myquizes" exact component={MyQuizes} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import {
+    Avatar,
+    Button,
+    Paper,
+    Grid,
+    Typography,
+    Container,
+} from "@material-ui/core";
+import useStyles from "./styles";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Input from "./Input";
+import { login, register } from "../../actions/auth";
+
+const initialState = {
+    userType: "",
+    firstName: "",
+    lastName: "",
+    userName: "",
+    mail: "",
+    password: "",
+    confirmPassword: "",
+};
+
+function Auth() {
+    const classes = useStyles();
+    const [isSignup, setIsSignup] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState(initialState);
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const isLanguageEnglish = useSelector((state) => state.language.isEnglish)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (isSignup) {
+            dispatch(register(formData, history))
+        } else {
+            dispatch(login(formData, history))
+        }
+    };
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    const handleShowPassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        setShowPassword(false);
+    };
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    {isSignup
+                        ? isLanguageEnglish
+                            ? "Sign up"
+                            : "Zarejestruj się"
+                        : isLanguageEnglish
+                            ? "Sign in"
+                            : "Zaloguj się"}
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {isSignup && (
+                            <>
+                                <Input
+                                    name="firstName"
+                                    label={isLanguageEnglish ? "First Name" : "Imię"}
+                                    handleChange={handleChange}
+                                    autoFocus
+                                    half
+                                />
+                                <Input
+                                    name="lastName"
+                                    label={isLanguageEnglish ? "Last Name" : "Nazwisko"}
+                                    handleChange={handleChange}
+                                    half
+                                />
+                                <Input
+                                    name="userType"
+                                    label={isLanguageEnglish ? "User type" : "Rodzaj konta"}
+                                    handleChange={handleChange}
+                                />
+                                <Input
+                                    name="mail"
+                                    label={isLanguageEnglish ? "Email address" : "Email"}
+                                    handleChange={handleChange}
+                                    type="email"
+                                />
+                            </>
+                        )}
+
+                        <Input
+                            name="userName"
+                            label={isLanguageEnglish ? "User Name" : "Nazwa użytkownika"}
+                            handleChange={handleChange}
+                        />
+                        <Input
+                            name="password"
+                            label={isLanguageEnglish ? "Password" : "Hasło"}
+                            handleChange={handleChange}
+                            type={showPassword ? "text" : "password"}
+                            handleShowPassword={handleShowPassword}
+                        />
+                        {isSignup && (
+                            <Input
+                                name="confirmPassword"
+                                label={isLanguageEnglish ? "Repeat password" : "Powtórz hasło"}
+                                handleChange={handleChange}
+                                type="password"
+                            />
+                        )}
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        {isSignup
+                            ? isLanguageEnglish
+                                ? "Sign up"
+                                : "Zarejestruj się"
+                            : isLanguageEnglish
+                                ? "Sign in"
+                                : "Zaloguj się"}
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup
+                                    ? isLanguageEnglish
+                                        ? "Already have an account? Sign in"
+                                        : "Masz już konto? Zaloguj się"
+                                    : isLanguageEnglish
+                                        ? "Don't have an account? Sign Up"
+                                        : "Nie masz konta? Zarejestruj się"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+export default Auth;
